8dc3ff06b054460295248d37bc8471cbc1ef5a70
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-06-02 21:05:20 +0600

    login page has been created

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <link rel="stylesheet" href="includes/bootstrap/bootstrap/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" rel="stylesheet" href="includes/bootstrap/bootstrap/css/style.css">
+</head>
+<body>
+    <h1>LOGIN SYSTEM IN PHP</h1>
+    <div class="container">
+    <form class="form">
+        <div class="form-group">
+            <label for="uname">Username</label>
+            <input type="text" name="uname" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="uname">Email</label>
+            <input type="email" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="submit" name="login" value="Login" class="btn btn-success">
+        </div>
+    </form>
+
+    </div>
+    
+    
+</body>
+</html>
